8db8f81b6df3aae27557b6a4728ffaaceec7d63d
dani7beth <danielledona28@gmail.com>
2020-12-14 14:44:03 -0700

    added backend and added a client react app  front end

--- app/controllers/api/restaurants_controller.rb
+++ app/controllers/api/restaurants_controller.rb
@@ -1,2 +1,11 @@
 class Api::RestaurantsController < ApplicationController
+    def index
+        render json: Restaurant.all
+    end
+
+    private
+
+    def restaurant_params
+        params.require(:restaurant).permit(:name)
+    end
 end
